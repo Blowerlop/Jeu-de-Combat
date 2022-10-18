28853d953d9252046f5358602e93912b1718748d
lisa hersant <lisa.hers11@gmail.com>
2022-10-18 16:44:51 +0200

    commit Lisa
    des fichiers

---
+++ Jeu de combat/MenuScene.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_de_combat
+{
+    public class MenuScene : MonoBehaviour, IScene
+    {
+        #region Variables
+
+        private List<MonoBehaviour> _componentsBehaviour = new List<MonoBehaviour>();
+
+        // Buttons
+        Button _startButton;
+        Button _quitButton;
+        private Texture2D _buttonSprite;
+
+
+        private SpriteFont _buttonFont;
+        private SpriteFont _titleFont;
+
+        #endregion
+
+        #region Updates
+
+        public MenuScene()
+        {
+            LoadSprites();
+            Instantiation();
+            AddComponentsBehaviour(_startButton, _quitButton);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _componentsBehaviour)
+                component.Update(gameTime);
+
+          
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (var component in _componentsBehaviour)
+                component.Draw(gameTime, spriteBatch);
+
+
+            float x = _titleFont.MeasureString("Le Jeu De La Bagarre").X / 2;
+            float y = _titleFont.MeasureString("Le Jeu De La Bagarre").Y / 2;
+            spriteBatch.DrawString(_titleFont, "Le Jeu De La Bagarre", new Vector2(Game1.instance.windowWidth / 2, 200), new Color(169,19,19), 0.0f, new Vector2(x,y), 1.0f, SpriteEffects.None, 0);
+        }
+        #endregion
+
+        #region Methods
+
+        public void LoadSprites()
+        {
+            _buttonSprite = Game1.instance.Content.Load<Texture2D>("Sprites/transparent");
+            _buttonFont = Game1.instance.Content.Load<SpriteFont>("Fonts/Button");
+            _titleFont = Game1.instance.Content.Load<SpriteFont>("Fonts/Title");
+        }
+
+        public void Instantiation()
+        {
+            _startButton = new Button(
+                _buttonSprite,
+                Game1.instance.windowCenter - Game1.instance.GetSpriteCenter(_buttonSprite) + new Vector2(0, -30),
+                "START",
+                _buttonFont
+                )
+            { textColor = new Color(169, 19, 19) };
+
+            _quitButton = new Button(
+                _buttonSprite,
+                Game1.instance.windowCenter - Game1.instance.GetSpriteCenter(_buttonSprite) + new Vector2(0, 50),
+                "QUIT",
+                _buttonFont
+                )
+            {
+                textColor = new Color(169, 19, 19)
+            };
+            
+
+            _startButton.Click += SceneManager.Selection;
+            _quitButton.Click += SceneManager.Quit;
+        }
+
+        public void AddComponentsBehaviour(params MonoBehaviour[] componentsBehaviour)
+        {
+            foreach (MonoBehaviour component in componentsBehaviour)
+            {
+                _componentsBehaviour.Add(component);
+            }
+        }
+        #endregion
+
+
+    }
+}
---
+++ Jeu de combat/SceneManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_de_combat
+{
+    public static class SceneManager
+    {
+        #region Methods
+
+        public static void ChangeState(MonoBehaviour scene)
+        {
+            Game1.instance._nextScene = scene;
+        }
+
+        public static void Start(object sender, EventArgs e)
+        {
+            Debug.WriteLine("StartScene");
+            ChangeState(new GameScene());
+        }
+        
+        public static void Menu()
+        {
+            ChangeState(new MenuScene());
+        }
+
+        public static void Selection(object sender, EventArgs e)
+        {
+            ChangeState(new PlayerSelectionScene());
+        }
+
+        public static void End()
+        {
+            ChangeState(new EndScene());
+        }
+
+        public static void Quit(object sender, EventArgs e)
+        {
+            Game1.instance.Exit();
+        }
+
+        #endregion
+    }
+}
